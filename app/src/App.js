80dcfc43ee85dd8a1657838db0527946795ffa38
kolserdav <serega12101983@gmail.com>
2022-04-20 18:55:05 +0700

    Added connect metamask script

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,4 @@
 import { useEffect } from 'react';
-import WalletConnectProvider from '@walletconnect/web3-provider';
-import logo from './logo.svg';
 import './App.css';
 
 const providerUrl = 'https://mainnet.infura.io/v3/cab3253649344bdc8709512c85be0c21';
@@ -8,13 +6,9 @@ const WALLET_LOCAL_STORAGE_NAME = 'wal';
 
 function App() {
 
-  useEffect(() => {
-    const metamask = document.querySelector('#metamask');
-    const mfpClose = document.querySelector('.mfp-close');
     function saveAccounts(accounts){
       // Если получены номера аккаунтов
       if (accounts?.length !== 0) {
-        mfpClose.click();
         alert(`Wallet connected ${JSON.stringify(accounts)}`);
         window.localStorage.setItem(WALLET_LOCAL_STORAGE_NAME, JSON.stringify(accounts));
       } else {
@@ -29,17 +23,7 @@ function App() {
       let accounts;
       const { ethereum } = window;
       // Если подключение к расширению браузера
-      if (ethereum?.isMetaMask) {
-        accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-      } else {
-        // Если подключение к мобильному устройству
-        const provider = new WalletConnectProvider({
-          rpc: {
-            1: providerUrl,
-          },
-        });
-        accounts = await provider?.enable();
-      }
+      accounts = await ethereum.request({ method: 'eth_requestAccounts' })
       /*
        TODO ...
        const NFT = new web3.eth.Contract(NFT_ABI, ac6);
@@ -62,24 +46,10 @@ function App() {
       // Если получены номера аккаунтов
       saveAccounts(accounts);
     }
-  }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={connectToMetamask}>connectToMetamask</button>
     </div>
   );
 }
